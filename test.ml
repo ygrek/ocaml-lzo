d44ff2d296d2d020b96e49cd5fe79e42c9246b4c
ygrek <ygrek@autistici.org>
2018-06-19 10:57:04 -0400

    fix build with OCaml >= 4.06.0 (fix #1)

--- test.ml
+++ test.ml
@@ -2,8 +2,11 @@
 let () =
   Random.self_init ();
   let check n =
-    let s = String.create n in
-    for i = 0 to pred n do s.[i] <- Char.chr (Random.int 256) done;
+    let s =
+      let s = Bytes.create n in
+      for i = 0 to pred n do Bytes.set s i (Char.chr (Random.int 256)) done;
+      Bytes.unsafe_to_string s
+    in
     assert (s = Lzo.decompress (Lzo.compress s) (String.length s));
   in
   for n = 0 to 1024 do check n done;
